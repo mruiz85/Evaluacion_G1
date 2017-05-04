f00cc9f9cec9f066c9d212a8f29ca19cd8d3295e
Marget <garasma2003@gmail.com>
2017-05-04 11:21:56 -0700

    Se creo un metodo Agregar
    Este metodo se encuentra en la capa Negocio para enlazar con la capa
    datos y presentacion el registro de un nuevo empleado

---
+++ Negocio/EmpleadoNegocio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class EmpleadoNegocio
+    {
+        public void Agregar(Entidad.Empleados empleados)
+        {
+            try
+            {
+                Datos.EmpleadoDatos dc = new Datos.EmpleadoDatos();
+                dc.Insert(empleados);
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+    }
+}
