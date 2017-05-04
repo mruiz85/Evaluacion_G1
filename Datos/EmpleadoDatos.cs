e45e7dd9b4b79ed7d31ff95f3e351ac63f70ca18
Marget <garasma2003@gmail.com>
2017-05-04 11:12:02 -0700

    Se creo un metodo insert
    En este metodo es para insertar a la base de datos por medio de la capa de datos

---
+++ Datos/EmpleadoDatos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class EmpleadoDatos
+    {
+        public void Insert(Entidad.Empleados e)
+        {
+            Entidad.BD_EvaluacionEntities dc = null;
+            try
+            {
+                dc = new Entidad.BD_EvaluacionEntities();
+                dc.Empleados.Add(e);
+                dc.SaveChanges(); 
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+    }
+}
